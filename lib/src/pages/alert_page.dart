6a8b548df2005544469ada8d329a01589c676ab9
hadiazb <hadiazb@unal.edu.co>
2021-11-08 07:30:57 -0500

    add more flutter  content

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -9,8 +9,53 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Alert Page'),
       ),
-      floatingActionButton: const FloatingActionButton(
-          child: Icon(Icons.ac_unit_sharp), onPressed: null),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => _mostrarAtert(context),
+          child: const Text('Mostrar Alerta!!!'),
+          style: ElevatedButton.styleFrom(
+              primary: Colors.blue, shape: const StadiumBorder()),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.ac_unit_sharp),
+          onPressed: () => _back(context)),
     );
+  }
+
+  void _back(BuildContext context) {
+    Navigator.of(context).pop();
+  }
+
+  void _mostrarAtert(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5.0)),
+            title: const Text('Titulo'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const <Widget>[
+                Text('Este es el contenido de la alerta!!!'),
+                FlutterLogo(
+                  size: 100.0,
+                )
+              ],
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: const Text('Cancelar')),
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('Ok')),
+            ],
+          );
+        },
+        barrierDismissible: true);
   }
 }
--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -7,8 +7,30 @@ class AvatarPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Avatar Page'),
+        title: const Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            margin: const EdgeInsets.all(5),
+            child: const CircleAvatar(
+                radius: 18,
+                backgroundImage: NetworkImage(
+                    'https://img.huffingtonpost.com/asset/5ee724ed2500004a2ceb34e3.jpeg?cache=MmZpXeEPFo&ops=1200_630')),
+          ),
+          Container(
+              margin: const EdgeInsets.only(right: 10),
+              child: const CircleAvatar(
+                child: Text('SL'),
+                radius: 18,
+                backgroundColor: Colors.brown,
+              )),
+        ],
       ),
+      body: const Center(
+          child: FadeInImage(
+        image: NetworkImage(
+            'https://img.huffingtonpost.com/asset/5ee724ed2500004a2ceb34e3.jpeg?cache=MmZpXeEPFo&ops=1200_630'),
+        placeholder: AssetImage('assets/jar-loading.gif'),
+      )),
     );
   }
 }
